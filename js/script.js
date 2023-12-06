edf04578a9db8c473a2d4c4dbbf94ae21b271431
Arnav Saini <saini.arnav.saini@gmail.com>
2023-12-05 23:28:49 -0500

    New Roster Section, more styling, custom scrollbar

--- js/script.js
+++ js/script.js
@@ -1,14 +1,43 @@
-$('.toggle').click(function () {
-    "use strict";
-    $('nav ul').slideToggle();
-});
 
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
 
+let active = 0;
+let lengthItems = items.length - 1;
 
-$(window).resize(function () {
-    "use strict";
-    if ($(window).width() > 780) {
-        $('nav ul').removeAttr('style');
+next.onclick = function(){
+    if(active + 1 > lengthItems){
+        active = 0;
+    }else{
+        active = active + 1;
+    }    
+    reloadSlider();
+}
+
+prev.onclick = function(){
+    if(active - 1 < 0){
+        active = lengthItems;
+    }else{
+        active = active - 1;
     }
-});
+    reloadSlider();
+}
 
+function reloadSlider(){
+    let checkLeft = items[active].offsetLeft;
+    list.style.left = -checkLeft + 'px';
+
+    let lastActiveDot = document.querySelector('.slider .dots li.active');
+    lastActiveDot.classList.remove('active');
+    dots[active].classList.add('active');
+}
+
+dots.forEach((li, key) =>{
+    li.addEventListener('click', function(){
+        active = key;
+        reloadSlider();
+    })
+})
